3027ad79138630c142bdcf5af860820d5191b46a
YukMattie <101512470+YukMattie@users.noreply.github.com>
2022-11-07 21:27:21 -0500

    Update 126.java

--- 126/126.java
+++ 126/126.java
@@ -1,1 +1,18 @@
-d
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+        char [] sArr = s.toCharArray();
+
+        int i = 0;
+        int j = s.length() - 1;
+        while(i <= j){
+            if (sArr[i] != sArr[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+}
