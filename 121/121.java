ca933bc88802b49045d5f410358240a7cf9a1b5a
YukMattie <101512470+YukMattie@users.noreply.github.com>
2022-11-07 21:28:58 -0500

    Create 121.java

---
+++ 121/121.java
@@ -0,0 +1,19 @@
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minNum = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(minNum > prices[i]){
+                minNum = prices[i];
+            }
+            else if(maxProfit < prices[i] - minNum){
+                maxProfit = prices[i] - minNum;
+            }
+        }
+        return maxProfit;
+        
+    }
+}
